bdbe8ef4d0c45075f858ec6942cb7673739f9616
Joey Dalu <josephdalughut@gmail.com>
2018-06-28 12:10:38 +0100

    Added, tested feature to show Fragments on MainActivity

---
+++ app/src/main/java/io/github/josephdalughut/journal/android/ui/activity/main/MainActivity.java
@@ -0,0 +1,47 @@
+package io.github.josephdalughut.journal.android.ui.activity.main;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.github.josephdalughut.journal.android.R;
+
+/**
+ * Our Main {@link android.app.Activity}, holds other visual components our users would interact with.
+ *
+ * Since the UI requirements of this application are fairly simple, we'll make use of only {@link android.app.Fragment}'s
+ * instead. They'll be hosted in a container view in this activity.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /** Our fragments would be added to this container */
+    private static final int LAYOUT_ID_FRAGMENT_CONTAINER = R.id.layFragmentContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //inflate our layout
+    }
+
+    /**
+     * Adds a fragment to the main activity Ui
+     * @param fragment a {@link Fragment} to add to the Ui
+     * @param replace if false, the fragment would be stacked above all other fragments in the container,
+     *                       otherwise it would replace all other fragments in the container.
+     */
+    public void addFragmentToUi(Fragment fragment, boolean replace){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction(); //get a transaction
+        if(replace){
+            transaction.replace(LAYOUT_ID_FRAGMENT_CONTAINER, fragment);
+        }else{
+            transaction.add(LAYOUT_ID_FRAGMENT_CONTAINER, fragment);
+
+            //adding this transaction to backstack would let us navigate
+            // back by pressing tbe back button
+            transaction.addToBackStack(null);
+        }
+        transaction.commit();
+    }
+
+}
---
+++ app/src/test/java/io/github/josephdalughut/journal/android/ui/activity/main/MainActivityTest.java
@@ -0,0 +1,79 @@
+package io.github.josephdalughut.journal.android.ui.activity.main;
+
+import android.support.v4.app.Fragment;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.controller.ActivityController;
+import org.robolectric.annotation.Config;
+
+import io.github.josephdalughut.journal.android.BuildConfig;
+
+import static org.junit.Assert.*;
+
+/**
+ * Joey Dalu (Joseph Dalughut)
+ * <a href="http://joeydalu.herokuapp.com">joeydalu.herokuapp.com</a>
+ * JournalApp
+ * 28/06/2018
+ *
+ * Unit testing {@link MainActivity}
+ */
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class MainActivityTest {
+
+    private MainActivity mActivity;
+    private ActivityController<MainActivity> mActivityController;
+
+    @Before
+    public void setUp() {
+        mActivityController = Robolectric.buildActivity(MainActivity.class);
+        mActivity = mActivityController.create().start().resume().get();
+    }
+
+    @After
+    public void tearDown() {
+        mActivityController.pause().stop().destroy();
+    }
+
+    @Test
+    public void notNull(){
+        assertNotNull(mActivity);
+    }
+
+    @Test
+    public void addFragmentToUi_ShowsFragment(){
+
+        //initialize and add a new fragment
+        mActivity.addFragmentToUi(new Fragment(), false);
+        assert mActivity.getSupportFragmentManager().getFragments().size() == 1;
+    }
+
+    @Test
+    public void addFragmentToUi_WithoutReplacing_KeepsPreviousFragment(){
+
+        //initialize, add two fragments
+        mActivity.addFragmentToUi(new Fragment(), false);
+        mActivity.addFragmentToUi(new Fragment(), false);
+
+        assert mActivity.getSupportFragmentManager().getFragments().size() == 2;
+
+    }
+
+    @Test
+    public void addFragmentToUi_WhenReplacing_ReplacesPreviousFragment(){
+
+        //initialize, add two fragments
+        mActivity.addFragmentToUi(new Fragment(), true);
+        mActivity.addFragmentToUi(new Fragment(), true);
+        assert mActivity.getSupportFragmentManager().getFragments().size() == 1;
+
+    }
+
+
+}
